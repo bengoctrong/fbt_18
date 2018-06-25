f0ad550e7e665e301a2f0cf4139f36d151fe52c5
suppasentai <sonduc2210@gmail.com>
2018-06-25 16:47:49 +0700

    comment review not done view va db

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,54 @@
 class ReviewsController < ApplicationController
+  before_action :logged_in_user, only: %i(create destroy)
+  before_action :load_comment_destroy, only: :destroy
+  before_action :load_comment_create, only: :create
+
   def create
-    @review = Review.new review_params
-    if @review.save
-      respond_to do |format|
-        format.html redirect_to @tour
+    respond_to do |format|
+      if @review.save
+        format.html{redirect_to @tour}
         format.js
+      else
+        flash[:danger] = t "review_failed"
+        format.html{redirect_to @tour}
       end
-    else
-      flash[:danger] = t "can't_review"
-      redirect_to @review
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @review.destroy
+        flash[:success] = t "delete_success_review"
+        format.html{redirect_to @tour}
+        format.js
+      else
+        flash[:danger] = t "delete_failed_review"
+        format.html{redirect_to @tour}
+      end
     end
   end
 
   private
+
+  def load_comment_destroy
+    if check_admin
+      @review = Review.find_by id: params[:id]
+      flash[:danger] = t "wrong_user" unless @review.present?
+    else
+      @review = current_user.reviews.find_by id: params[:id]
+      flash[:danger] = t "no review" unless @review.present
+    end
+    @tour = @review.tour
+  end
+
+  def load_comment_create
+    @tour = Tour.find_by id: params[:review][:tour_id]
+    if @tour
+      @review = @tour.reviews.build review_params
+    else
+      flash[:danger] =  t "load_tour_failed"
+      redirect_to root_path
+  end
 
   def review_params
     params.require(:review).permit :tour_id, :content, :user_id
